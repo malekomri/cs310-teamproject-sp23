6aae6dd8d6ab37ac3d5fe9e08f19f890f636d965
Malek Omri <omrimalektn@gmail.com>
2023-03-05 11:07:19 -0600

    Added Map to shift class

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/Shift.java
@@ -1,9 +1,10 @@
 package edu.jsu.mcis.cs310.tas_sp23.dao;
 
 import java.time.LocalTime;
+import java.util.Map;
 
 public class Shift {
-
+    
     private int id;
     private String description;
     private LocalTime start;
@@ -14,57 +15,117 @@ public class Shift {
     private LocalTime lunchStart;
     private LocalTime lunchStop;
     private int lunchDeduct;
-
-    public Shift(int id, String description, LocalTime start, LocalTime stop, int interval, int gracePeriod, int dock, LocalTime lunchStart, LocalTime lunchStop, int lunchDeduct) {
-        this.id = id;
-        this.description = description;
-        this.start = start;
-        this.stop = stop;
-        this.interval = interval;
-        this.gracePeriod = gracePeriod;
-        this.dock = dock;
-        this.lunchStart = lunchStart;
-        this.lunchStop = lunchStop;
-        this.lunchDeduct = lunchDeduct;
+    private int shiftDuration;
+    private int lunchDuration;
+    
+    public Shift(Map<String, String> params) {
+        this.id = Integer.parseInt(params.get("id"));
+        this.description = params.get("description");
+        this.start = LocalTime.parse(params.get("start"));
+        this.stop = LocalTime.parse(params.get("stop"));
+        this.interval = Integer.parseInt(params.get("interval"));
+        this.gracePeriod = Integer.parseInt(params.get("graceperiod"));
+        this.dock = Integer.parseInt(params.get("dock"));
+        this.lunchStart = LocalTime.parse(params.get("lunchstart"));
+        this.lunchStop = LocalTime.parse(params.get("lunchstop"));
+        this.lunchDeduct = Integer.parseInt(params.get("lunchdeduct"));
+        this.shiftDuration = Integer.parseInt(params.get("shiftduration"));
+        this.lunchDuration = Integer.parseInt(params.get("lunchduration"));
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public LocalTime getStart() {
         return start;
     }
 
+    public void setStart(LocalTime start) {
+        this.start = start;
+    }
+
     public LocalTime getStop() {
         return stop;
+    }
+
+    public void setStop(LocalTime stop) {
+        this.stop = stop;
     }
 
     public int getInterval() {
         return interval;
     }
 
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
     public int getGracePeriod() {
         return gracePeriod;
+    }
+
+    public void setGracePeriod(int gracePeriod) {
+        this.gracePeriod = gracePeriod;
     }
 
     public int getDock() {
         return dock;
     }
 
+    public void setDock(int dock) {
+        this.dock = dock;
+    }
+
     public LocalTime getLunchStart() {
         return lunchStart;
+    }
+
+    public void setLunchStart(LocalTime lunchStart) {
+        this.lunchStart = lunchStart;
     }
 
     public LocalTime getLunchStop() {
         return lunchStop;
     }
 
+    public void setLunchStop(LocalTime lunchStop) {
+        this.lunchStop = lunchStop;
+    }
+
     public int getLunchDeduct() {
         return lunchDeduct;
+    }
+
+    public void setLunchDeduct(int lunchDeduct) {
+        this.lunchDeduct = lunchDeduct;
+    }
+
+    public int getLunchDuration() {
+        return lunchDuration;
+    }
+
+    public void setLunchDuration(int lunchDuration) {
+        this.lunchDuration = lunchDuration;
+    }
+
+    public int getShiftDuration() {
+        return shiftDuration;
+    }
+
+    public void setShiftDuration(int shiftDuration) {
+        this.shiftDuration = shiftDuration;
     }
 }
