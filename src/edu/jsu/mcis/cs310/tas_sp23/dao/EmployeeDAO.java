ae5647f2dbf949208f62a230a50f858352b20f57
Malek Omri <omrimalektn@gmail.com>
2023-03-10 00:01:04 -0600

    Added Employee and EmployeeDAO - Department problem

---
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/EmployeeDAO.java
@@ -0,0 +1,114 @@
+package edu.jsu.mcis.cs310.tas_sp23.dao;
+
+import edu.jsu.mcis.cs310.tas_sp23.Employee;
+import edu.jsu.mcis.cs310.tas_sp23.Badge;
+import edu.jsu.mcis.cs310.tas_sp23.EmployeeType;
+import edu.jsu.mcis.cs310.tas_sp23.Shift;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+
+public class EmployeeDAO {
+    
+    private static final String QUERY_FIND_BY_ID = "SELECT * FROM employee WHERE id = ?";
+    
+    private final DAOFactory daoFactory;
+    
+    public EmployeeDAO(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+
+    public Employee find(int id) {
+        Employee employee = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            Connection conn = daoFactory.getConnection();
+            if (conn.isValid(0)) {
+                ps = conn.prepareStatement(QUERY_FIND_BY_ID);
+                ps.setInt(1, id);
+                boolean hasResults = ps.execute();
+                if (hasResults) {
+                    rs = ps.getResultSet();
+                    if (rs.next()) {
+                        String firstName = rs.getString("firstname");
+                        String middleName = rs.getString("middlename");
+                        String lastName = rs.getString("lastname");
+                        String badgeId = rs.getString("badgeid");
+                        int typeId = rs.getInt("employeetypeid");
+                        int departmentId = rs.getInt("departmentid");
+                        LocalDateTime active = rs.getTimestamp("active").toLocalDateTime();
+                        BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
+                        Badge badge = badgeDAO.find(badgeId);
+                        DepartmentDAO departmentDAO = daoFactory.getDepartmentDAO();
+                        Department department = departmentDAO.find(departmentId);
+                        EmployeeType type = EmployeeType.values()[typeId - 1];
+                        employee = new Employee(id, firstName, middleName, lastName, active, badge, department, type);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e.getMessage());
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+        }
+        return employee;
+    }
+    
+    public Employee find(Badge badge) {
+
+        Employee employee = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        
+        try (Connection conn = daoFactory.getConnection();) {
+            
+            pstmt = conn.prepareStatement("SELECT * FROM employee WHERE badgeid = ?");
+            pstmt.setString(1, badge.getId());
+            rs = pstmt.executeQuery();
+            
+            if (rs.next()) {
+                
+                int id = rs.getInt("id");
+                String firstname = rs.getString("firstname");
+                String middlename = rs.getString("middlename");
+                String lastname = rs.getString("lastname");
+                LocalDateTime active = rs.getTimestamp("active").toLocalDateTime();
+                int employeeTypeInt = rs.getInt("employeetypeid");
+                int departmentId = rs.getInt("departmentid");
+                
+                DepartmentDAO departmentDAO = daoFactory.getDepartmentDAO();
+                Department department = departmentDAO.find(departmentId);
+                
+                ShiftDAO shiftDAO = daoFactory.getShiftDAO();
+                Shift shift = shiftDAO.find(id);
+                
+                EmployeeType employeeType = EmployeeType.values()[employeeTypeInt];
+                
+                employee = new Employee(id, firstname, middlename, lastname, active, badge, department, shift, employeeType);
+                
+            }
+        } catch (SQLException e) {
+            throw new DAOException(e.getMessage());
+        }
+        
+        if (employee == null) {
+            throw new DAOException("No employee found with badge ID " + badge.getId());
+        }
+        
+        return employee;
+    }
+    
---
+++ src/edu/jsu/mcis/cs310/tas_sp23/employee.java
@@ -0,0 +1,70 @@
+import java.time.LocalDateTime;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private LocalDateTime active;
+    private Badge badge;
+    private Department department;
+    private Shift shift;
+    private EmployeeType type;
+
+    public Employee(int id, String firstName, String middleName, String lastName, LocalDateTime active, Badge badge, Department department, Shift shift, EmployeeType type) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.active = active;
+        this.badge = badge;
+        this.department = department;
+        this.shift = shift;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDateTime getActive() {
+        return active;
+    }
+
+    public Badge getBadge() {
+        return badge;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public Shift getShift() {
+        return shift;
+    }
+
+    public EmployeeType getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        String name = String.format("%s%s%s", firstName, middleName != null ? " " + middleName + " " : " ", lastName);
+        String badgeDescription = badge.getDescription() != null ? badge.getDescription() : "";
+        String badgeId = badge.getId() != null ? badge.getId() : "";
+        String departmentDescription = department.getDescription() != null ? department.getDescription() : "";
+        return String.format("#%d %s (%s, %s) [%s] (%s) %s", id, name, badgeDescription, badgeId, type.toString(), departmentDescription, active.toString());
+    }
+}
